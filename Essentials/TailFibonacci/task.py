6247d321ea720ec0d9298823b447aaf04b54613b
Volodymyr Butko <volodymyr_butko@epam.com>
2025-03-17 13:44:28 +0000

    Add implementation of the TailFibonacci task

--- Essentials/TailFibonacci/task.py
+++ Essentials/TailFibonacci/task.py
@@ -4,4 +4,20 @@ from Essentials.TailFibonacci.tail_recursion import tail_call_optimized
 
 
 def fibonacci_impl() -> Callable[[int], int]:
-    raise Exception('Implement me')
+    def fibonacci(n: int) -> int:
+        if n < 0:
+            return (-1) ** (abs(n) + 1) * fibonacci(abs(n))
+        elif n == 0:
+            return 0
+        elif n == 1:
+            return 1
+
+        @tail_call_optimized
+        def tail_rec(i: int, last: int, next_to_last: int) -> int:
+            if i >= n:
+                return last
+            return tail_rec(i + 1, last + next_to_last, last)
+
+        return tail_rec(2, 1, 1)
+
+    return fibonacci
