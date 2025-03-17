3404f49615a33d82b6b8fe6050b474342686649d
Volodymyr Butko <volodymyr_butko@epam.com>
2025-03-17 13:45:29 +0000

    Add tests for the TailFibonacci task

--- Essentials/TailFibonacci/tests/test_task.py
+++ Essentials/TailFibonacci/tests/test_task.py
@@ -22,3 +22,16 @@ class TestCase(unittest.TestCase):
             current = fibonacci_impl()(rnd)
             next_el = fibonacci_impl()(rnd + 1)
             self.assertEqual(prev + current, next_el, msg=f"fib of {next_el} should be {prev} + {current}")
+
+    def test_fib_negative(self):
+        self.assertEqual(fibonacci_impl()(-10), -55, msg="fib of -10 should be -55")
+
+    def test_fib_large(self):
+        self.assertEqual(fibonacci_impl()(50), 12586269025, msg="fib of 50 should be 12586269025")
+
+    def test_fib_very_large(self):
+        self.assertEqual(fibonacci_impl()(100), 354224848179261915075, msg="fib of 100 should be 354224848179261915075")
+
+    def test_fib_very_very_large(self):
+        self.assertEqual(fibonacci_impl()(300), 222232244629420445529739893461909967206666939096499764990979600,
+                         msg="fib of 300 should be 222232244629420445529739893461909967206666939096499764990979600")
