31c8e227bb7ccfee5bd889b4be8d0d4fb3d51675
Volodymyr Butko <volodymyr_butko@epam.com>
2025-03-17 12:50:59 +0000

    Add implementation of the Factorial task

--- Essentials/Factorial/task.py
+++ Essentials/Factorial/task.py
@@ -4,4 +4,8 @@ from Essentials.Factorial.tail_recursion import tail_call_optimized
 
 
 def factorial_impl() -> Callable[[int], int]:
-    raise Exception('Implement me')
+    @tail_call_optimized
+    def factorial(n: int, acc: int = 1) -> int:
+        return acc if n <= 0 else factorial(n - 1, n * acc)
+
+    return factorial
