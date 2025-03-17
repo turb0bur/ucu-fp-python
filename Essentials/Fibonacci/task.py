82498222d4d077c9330db79e71315b6aeb9fc699
Volodymyr Butko <volodymyr_butko@epam.com>
2025-03-17 13:27:10 +0000

    Add implementation of the Fibonacci task

--- Essentials/Fibonacci/task.py
+++ Essentials/Fibonacci/task.py
@@ -2,4 +2,12 @@ from typing import Callable
 
 
 def fibonacci_impl() -> Callable[[int], int]:
-    raise Exception("Implement me")
+    def fibonacci(n: int) -> int:
+        if n < 0:
+            return (-1)**(abs(n)+1) * fibonacci(abs(n))
+        elif n < 2:
+            return n
+        else:
+            return fibonacci(n - 1) + fibonacci(n - 2)
+    
+    return fibonacci
